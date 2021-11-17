a0f74bde35c5b84bcdd425d8d7dd267def80a539
kevcisme <kevcisme>
2021-11-17 09:48:10 +0000

    UPDATE demo

---
+++ Source/01 - Populate a Feature Store.py
@@ -0,0 +1,179 @@
+# Databricks notebook source
+# MAGIC %md-sandbox
+# MAGIC 
+# MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
+# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
+# MAGIC </div>
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Create Feaures in Feature Store
+# MAGIC 
+# MAGIC In this notebook, we're going to populate a feaure store.
+
+# COMMAND ----------
+
+#%run ./Includes/Classroom-Setup
+
+# COMMAND ----------
+
+from databricks import feature_store
+from pyspark.sql.functions import *
+from pyspark.sql.types import FloatType, IntegerType, StringType
+from pytz import timezone
+ 
+ 
+@udf(returnType=IntegerType())
+def is_weekend(dt):
+    tz = "America/New_York"
+    return int(dt.astimezone(timezone(tz)).weekday() >= 5)  # 5 = Saturday, 6 = Sunday
+  
+@udf(returnType=StringType())  
+def partition_id(dt):
+    # datetime -> "YYYY-MM"
+    return f"{dt.year:04d}-{dt.month:02d}"
+ 
+ 
+def filter_df_by_ts(df, ts_column, start_date, end_date):
+    if ts_column and start_date:
+        df = df.filter(col(ts_column) >= start_date)
+    if ts_column and end_date:
+        df = df.filter(col(ts_column) < end_date)
+    return df
+
+# COMMAND ----------
+
+def pickup_features_fn(df, ts_column, start_date, end_date):
+    """
+    Computes the pickup_features feature group.
+    To restrict features to a time range, pass in ts_column, start_date, and/or end_date as kwargs.
+    """
+    df = filter_df_by_ts(
+        df, ts_column, start_date, end_date
+    )
+    pickupzip_features = (
+        df.groupBy(
+            "pickup_zip", window("tpep_pickup_datetime", "1 hour", "15 minutes")
+        )  # 1 hour window, sliding every 15 minutes
+        .agg(
+            mean("fare_amount").alias("mean_fare_window_1h_pickup_zip"),
+            count("*").alias("count_trips_window_1h_pickup_zip"),
+        )
+        .select(
+            col("pickup_zip").alias("zip"),
+            unix_timestamp(col("window.end")).alias("ts").cast(IntegerType()),
+            partition_id(to_timestamp(col("window.end"))).alias("yyyy_mm"),
+            col("mean_fare_window_1h_pickup_zip").cast(FloatType()),
+            col("count_trips_window_1h_pickup_zip").cast(IntegerType()),
+        )
+    )
+    return pickupzip_features
+  
+def dropoff_features_fn(df, ts_column, start_date, end_date):
+    """
+    Computes the dropoff_features feature group.
+    To restrict features to a time range, pass in ts_column, start_date, and/or end_date as kwargs.
+    """
+    df = filter_df_by_ts(
+        df,  ts_column, start_date, end_date
+    )
+    dropoffzip_features = (
+        df.groupBy("dropoff_zip", window("tpep_dropoff_datetime", "30 minute"))
+        .agg(count("*").alias("count_trips_window_30m_dropoff_zip"))
+        .select(
+            col("dropoff_zip").alias("zip"),
+            unix_timestamp(col("window.end")).alias("ts").cast(IntegerType()),
+            partition_id(to_timestamp(col("window.end"))).alias("yyyy_mm"),
+            col("count_trips_window_30m_dropoff_zip").cast(IntegerType()),
+            is_weekend(col("window.end")).alias("dropoff_is_weekend"),
+        )
+    )
+    return dropoffzip_features  
+
+# COMMAND ----------
+
+from datetime import datetime
+ 
+pickup_features = pickup_features_fn(
+    raw_data, ts_column="tpep_pickup_datetime", start_date=datetime(2016, 1, 1), end_date=datetime(2016, 1, 31)
+)
+dropoff_features = dropoff_features_fn(
+    raw_data, ts_column="tpep_dropoff_datetime", start_date=datetime(2016, 1, 1), end_date=datetime(2016, 1, 31)
+)
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC CREATE DATABASE IF NOT EXISTS feature_store_taxi_example;
+
+# COMMAND ----------
+
+fs = feature_store.FeatureStoreClient()
+
+# COMMAND ----------
+
+spark.conf.set("spark.sql.shuffle.partitions", "5")
+ 
+fs.create_feature_table(
+    name="feature_store_taxi_example.trip_pickup_features",
+    keys=["zip", "ts"],
+    features_df=pickup_features,
+    partition_columns="yyyy_mm",
+    description="Taxi Fares. Pickup Features",
+)
+fs.create_feature_table(
+    name="feature_store_taxi_example.trip_dropoff_features",
+    keys=["zip", "ts"],
+    features_df=dropoff_features,
+    partition_columns="yyyy_mm",
+    description="Taxi Fares. Dropoff Features",
+)
+
+# COMMAND ----------
+
+# Compute the pickup_features feature group.
+pickup_features_df = pickup_features_fn(
+  df=raw_data,
+  ts_column="tpep_pickup_datetime",
+  start_date=datetime(2016, 2, 1),
+  end_date=datetime(2016, 2, 29),
+)
+ 
+# Write the pickup features DataFrame to the feature store table
+fs.write_table(
+  name="feature_store_taxi_example.trip_pickup_features",
+  df=pickup_features_df,
+  mode="merge",
+)
+ 
+# Compute the dropoff_features feature group.
+dropoff_features_df = dropoff_features_fn(
+  df=raw_data,
+  ts_column="tpep_dropoff_datetime",
+  start_date=datetime(2016, 2, 1),
+  end_date=datetime(2016, 2, 29),
+)
+ 
+# Write the dropoff features DataFrame to the feature store table
+fs.write_table(
+  name="feature_store_taxi_example.trip_dropoff_features",
+  df=dropoff_features_df,
+  mode="merge",
+)
+
+# COMMAND ----------
+
+fs.write_table(
+  name="feature_store_taxi_example.trip_pickup_features",
+  df=pickup_features_df,
+  mode="overwrite",
+)
+
+# COMMAND ----------
+
+fs.write_table(
+  name="streaming_example.streaming_features",
+  df=streaming_df,
+  mode="merge",
+)
